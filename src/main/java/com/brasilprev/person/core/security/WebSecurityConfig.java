136229bed765bb3696589273a43e3b920e0336c0
titiscms <titisbackup@gmail.com>
2020-09-15 03:01:10 -0300

    Implementing Basic Security in the API

---
+++ src/main/java/com/brasilprev/person/core/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.brasilprev.person.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.and()
+				.authorizeRequests()
+					.anyRequest().authenticated()
+			.and()
+				.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.csrf().disable();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
