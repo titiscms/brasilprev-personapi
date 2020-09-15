2d30c7e0d8a2ab49b349275eb7c32388e4a410af
titiscms <titisbackup@gmail.com>
2020-09-15 04:23:10 -0300

    Refactoring files

--- src/main/java/com/brasilprev/person/core/security/WebSecurityConfig.java
+++ src/main/java/com/brasilprev/person/core/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.brasilprev.person.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("brasilprev")
+					.password(passwordEncoder().encode("personapi"))
+					.roles("ADMIN")
+				.and()
+				.withUser("manager")
+					.password(passwordEncoder().encode("thiagomanager"))
+					.roles("ADMIN");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
